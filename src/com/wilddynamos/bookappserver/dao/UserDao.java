6f30f37c27519df298740c25660c39737c2023fa
ZheQian <quizip@gmail.com>
2013-11-24 00:29:31 -0800

    modify editprofileservlet

--- src/com/wilddynamos/bookappserver/dao/UserDao.java
+++ src/com/wilddynamos/bookappserver/dao/UserDao.java
@@ -7,7 +7,7 @@ import com.wilddynamos.bookappserver.model.User;
 
 public class UserDao {
 	
-	public static final String address = "jdbc:mysql://127.0.0.1:3306/book_app";
+	public static final String address = "jdbc:mysql://192.168.1.7:3306/book_app";
 	
 	private static final Integer DEFAULT_USER_PAGESIZE = 10;
 	private Connection conn;
@@ -16,7 +16,7 @@ public class UserDao {
 	public UserDao() {
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
-			conn = DriverManager.getConnection(address, "root", null);
+			conn = DriverManager.getConnection(address, "zhe", null);
 			stmt = conn.createStatement();
 		} catch (Exception e) {
 			//TODO
@@ -75,7 +75,7 @@ public class UserDao {
 	
 	public int update(User user) {
 		try {
-			return stmt.executeUpdate("UPDATE user SET"
+			return stmt.executeUpdate("UPDATE user SET "
 									   + "email = '" + user.getEmail() + "', "
 									   + "password = '" + user.getPassword() + "', "
 									   + "name = '" + user.getName() + "', "
@@ -83,7 +83,7 @@ public class UserDao {
 									   + "campus = '" + user.getCampus() + "', "
 									   + "contact = '" + user.getContact() + "', "
 									   + "address = '" + user.getAddress() + "', "
-									   + "photo_path = '" + user.getPhotoPath() + "', "
+									   + "photo_path = '" + user.getPhotoPath() + "' "
 									   + "WHERE id = '" + user.getId() + "';");
 		} catch(Exception e) {
 			e.printStackTrace();
